2be1386309f69cb0b26ed4ce3b888f7bc75f53ec
Chris Pressey <cpressey@catseye.tc>
2023-04-22 10:51:03 +0100

    Under 64-bit OS, `ozz` must be built as 32-bit executable to work.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh -ex
 
 cd src
 
@@ -52,12 +52,26 @@ if [ `uname -s` = 'Linux' ]; then
     LDL='-ldl'
 fi
 
+# ozz doesn't work as a 64-bit executable, so we build it as 32-bit
+# by passing the `-m32` flag to gcc.
+# ---> For more info, see, e.g., https://stackoverflow.com/q/3501878
+# This requires 32-bit headers installed.  Under Ubuntu:
+#   sudo apt install gcc-multilib
+# ---> For more info, see, e.g., https://stackoverflow.com/q/54082459
+# This will also require 32-bit versions of the libraries that
+# we link to.  Under Ubuntu:
+#   sudo dpkg --add-architecture i386
+#   sudo apt update
+#   sudo apt install libc6:i386 libedit-dev:i386
+
+CFLAGS=-m32
+
 OBJECTS=""
 for SOURCE in $SOURCES; do
-  gcc -DVERSION='"1.0.4-4ce1"' $DREADLINE -c $SOURCE.c -o $SOURCE.o || exit 1
+  gcc $CFLAGS -DVERSION='"1.0.4-4ce1"' $DREADLINE -c $SOURCE.c -o $SOURCE.o || exit 1
   OBJECTS="$OBJECTS $SOURCE.o"
 done
 
-gcc $OBJECTS -o ozz $LDL $LREADLINE
+gcc $CFLAGS $OBJECTS -o ozz $LDL $LREADLINE
 
 cd ..
